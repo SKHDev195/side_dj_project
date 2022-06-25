a138e72eeb01bd84f6383a5f07a846d58759c6fa
Shukhrat Khannanov <khannanov.sh@gmail.com>
2022-06-25 16:22:54 +0600

    Commit before working with models

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,12 +1,34 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+projects_list = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'This wasa project where I built out my portfolio'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'Awesome open source project I am still working on'
+    },
+]
 
 
 def projects(request):
-    return render(request, 'projects.html')
+    page = 'projects'
+    number = 11
+    context = {'page': page, 'number': number, 'projects': projects_list}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    project_obj = None
+    for i in projects_list:
+        if i['id'] == pk:
+            project_obj = i
+    return render(request, 'projects/single-project.html', {'project': project_obj})
